c526ae0061c9c4f6450640a7f66d4c740ca767b2
RenataMikh <renata.mikh@yahoo.com>
2026-02-12 20:44:49 +0300

    Reapply "main.cpp"
    This reverts commit 6dc0ccaaff3f71fede3ed54c5ec1638d35aaf6d4.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 using namespace std;
-
 int main() {
     int n;
     cin >> n;
-    
     int count = 0;
     for (int i = 0; i < n; i++) {
         int a, b, c;
@@ -13,7 +11,6 @@ int main() {
             count++;
         }
     }
-    
     cout << count << endl;
     return 0;
 }
